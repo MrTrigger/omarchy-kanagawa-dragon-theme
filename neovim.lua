76a1a5a6a6f57b43b114bfb614e778f0504d26db
Magnus Wallin <magnus.wallin1@gmail.com>
2025-08-31 18:26:23 +0200

    Fixed neovim plugin

--- neovim.lua
+++ neovim.lua
@@ -1,3 +1,17 @@
 return {
-  colorscheme = "kanagawa-dragon"
+  {
+    "rebelot/kanagawa.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("kanagawa").setup({
+        theme = "dragon", -- Load the dragon variant
+        background = {
+          dark = "dragon",
+          light = "lotus"
+        },
+      })
+      vim.cmd("colorscheme kanagawa-dragon")
+    end,
+  },
 }
